ec46570f3f00aa877e9f85a16acc7e0b07c88352
Toddman0430 <ctemory7@gmail.com>
2014-10-01 22:28:25 -0500

    Submission.Update_quality had one long method. Extracted methods to reduce maintenance implications and conditional nesting. Specs passing.

--- update_quality.rb
+++ update_quality.rb
@@ -3,37 +3,35 @@ require 'award'
 def update_quality(awards)
   awards.each do |award|
     initial_quality = award.quality
-    name = award.name
     compute_with_blue_first_and_compare(award)
-    award.expires_in -= 1   unless blue_distinction_plus?(name)
+    award.expires_in -= 1 unless blue_distinction_plus?(award.name)
     factor_in_expiration_dates(award)
     quality_decrease = initial_quality - award.quality
-    award.quality -= quality_decrease if blue_star?(name) && quality_decrease >= 1
+    award.quality -= quality_decrease if blue_star?(award.name) && quality_decrease >= 1
   end
 end
 
 def compute_with_blue_first_and_compare(award)
-  if (!blue_first?(award.name) && !blue_compare?(award.name))
-    (award.quality -= 1 unless blue_distinction_plus?(award.name) if award.quality > 0)
-  else
-    if quality_ceiling?(award.quality)
-      award.quality += 1
-      if blue_compare?(award.name)
-          award.quality += 1 if award.expires_in < 11 if quality_ceiling?(award.quality)
-          award.quality += 1 if award.expires_in < 6 if quality_ceiling?(award.quality)
+  ceiling = quality_ceiling?(award.quality)
+  !blue_first?(award.name) && !blue_compare?(award.name) ?
+      (award.quality -= 1 if !blue_distinction_plus?(award.name) if award.quality > 0) :
+      if ceiling
+        award.quality += 1
+        if blue_compare?(award.name)
+          award.quality += 1 if ceiling if award.expires_in < 11
+          award.quality += 1 if ceiling if award.expires_in < 6
+        end
       end
-    end
-  end
 end
 
 def factor_in_expiration_dates(award)
-  if (!blue_first?(award.name))
-    (!blue_compare?(award.name)) ?
-        award.quality -= 1 unless blue_distinction_plus?(award.name) if award.quality > 0 :
-        award.quality = award.quality - award.quality
-  else
-    award.quality += 1
-  end if quality_ceiling?(award.quality) if award.expires_in < 0
+  !blue_first?(award.name) ?
+      !blue_compare?(award.name) ?
+          if award.quality > 0
+              award.quality -= 1 unless blue_distinction_plus?(award.name)
+          end :
+          award.quality = award.quality - award.quality :
+      award.quality += 1 unless !quality_ceiling?(award.quality) if award.expires_in < 0
 end
 
 def quality_ceiling?(quality)
